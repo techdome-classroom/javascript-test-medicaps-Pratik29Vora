1a33696b9a43ef2d05b18bfe4ad5f7fc2e30e6d4
Pratik29Vora <pratikvora1911@gmail.com>
2024-04-25 13:21:48 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,5 +1,23 @@
 function longestSubstring(s) {
     // Implementation of longestSubstring function
+    let start = 0;
+  let end = 0;
+  let maxLength = 0;
+  let seen = {};
+
+  while (end < str.length) {
+    const currentChar = str[end];
+    if (seen[currentChar]) {
+      start = Math.max(start, seen[currentChar] + 1);
+    }
+
+    seen[currentChar] = end;
+    maxLength = Math.max(maxLength, end - start + 1);
+
+    end++;
+  }
+
+  return str.substring(start, end);
 }
 
 module.exports = { longestSubstring };
